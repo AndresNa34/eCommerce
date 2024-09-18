bd61b0248255c14f465866b96c1d1c54cde0cc2b
andres-na-dev-admin <manazutec@gmail.com>
2024-09-18 15:20:16 -0600

    AndresNa: migrate to new syntaxis, ng g @angular:core control-flow

--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "@shared/components/header/header.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
